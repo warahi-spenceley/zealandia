ff72f0a1fc41602648c924036db290c88c00afa7
Warahi Spenceley <wizzzo@protonmail.com>
2021-03-15 20:31:57 +1300

    linting

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -11,26 +11,23 @@ function App (props) {
   return (
     <>
       <div className='app'>
-        <div className="container">
+        <div className="container-fluid">
           <div className="row">
             <div className="col-sm-2">
               <img src="/images/logo.png" alt="app-logo" className="logo"/>
-            </div> 
-          
-            <div className="col-sm-10 pe-3 py-3 ps-3 ps-md-0">
+            </div>
+            <div className="search-bar col-sm-10 pe-3 py-3 ps-3 ps-md-0">
               <form className="bg-white shadow-sm rounded-3 px-3 pb-3 pt-2 text-midnight">
-              <input
-                type="text"
-                className="form-control"
-                name="search"
-                placeholder="search"
-              />
+                <input
+                  type="text"
+                  className="form-control"
+                  name="search"
+                  placeholder="search"
+                />
               </form>
             </div>
-
           </div>
         </div>
-
 
         <div className="north-island float-end">
           <img src="/images/north_island.png" alt="north-island-link"/>
