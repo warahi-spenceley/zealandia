59c368cebd2f30c79a6dd2ea6433a17af6f96823
Warahi Spenceley <wizzzo@protonmail.com>
2021-03-16 17:46:49 +1300

    linting

--- client/apis/wwo.js
+++ client/apis/wwo.js
@@ -1,21 +1,13 @@
 import request from 'superagent'
 
-const baseUrl = 'http://api.worldweatheronline.com/premium/v1/marine.ashx?key='
+const baseUrl = 'http://api.worldweatheronline.com/premium/v1/marine.ashx'
 const format = '&format=json'
 const tide = '&tide=yes'
-
-// export function getWaimaramaData () {
-//   const coors = '-39.8148531,176.9923951'
-//   return request.get(`${baseUrl}?key=${process.env.REACT_APP_WWO_KEY}&q=${coors}fx=yes&tide=yes`)
-//     .then(res => {
-//       console.log(res.body)
-//       return res.body
-//     })
-// }
+const key = '?&key=e2d788c2011f4c7787c13418211603'
 
 export function getWaimaramaData () {
   const coors = '&q=-39.8148531,176.9923951'
-  return request.get(`${baseUrl}${process.env.REACT_APP_WWO_KEY}${format}${coors}${tide}`)
+  return request.get(`${baseUrl}${key}${format}${coors}${tide}`)
     .then(res => {
       console.log(res.body)
       return res.body
