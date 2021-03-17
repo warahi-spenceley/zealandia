94628da51776a007c77db343384217d8a2e22417
Warahi Spenceley <wizzzo@protonmail.com>
2021-03-18 10:07:45 +1300

    add some table headers

--- client/components/WaimaramaBeach.jsx
+++ client/components/WaimaramaBeach.jsx
@@ -11,43 +11,52 @@ function WaimaramaBeach ({ dispatch, waimarama }) {
     <>
       <h1>Waimarama Beach</h1>
       <table>
-        <tr>
           {waimarama.map(day => (
             <>
             <tr>
-            <td>{day.date}</td>
+            <th>{day.date}</th>
             </tr>
               {day.hourly.map(hour => (
                 <>
                   <tr>
-                    <td>Time: {hour.time}</td>
+                    <th>Time</th>
+                    <th>swell</th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th>Wind</th>
+                    <th></th>
+                    <th></th>
+                    <th>Gust</th>
+                    <th>{hour.weatherDesc.map(desc => (
+                    <td>{desc.value}</td>
+                  ))}</th>
                   </tr>
                   <tr>
+                    <td>{hour.time}</td>
                     <td>{hour.swellHeight_ft} ft</td>
                     <td>{hour.swellDir16Point}</td>
                     <td>{hour.swellDir}&#176;</td>
                     <td>{hour.swellPeriod_secs}s</td>
                     <td>{hour.sigHeight_m}m</td>
-                  </tr>
-                  <tr>
+
                     <td>{hour.winddir16Point}</td>
                     <td>{hour.winddirDegree}&#176;</td>
                     <td>{hour.windspeedKmph} k/ph</td>
                     <td>{hour.WindGustKmph} k/ph</td>
-                  </tr>
-                  {hour.weatherDesc.map(desc => (
-                    <tr>{desc.value}</tr>
-                  ))}
+
                   {hour.weatherIconUrl.map(icon => (
-                    <tr>
+                    <td>
                       <img src={icon.value} alt="weather icon"></img>
-                    </tr>
+                    </td> 
                   ))}
+                  </tr>
                 </>
               ))}
             </>
           ))}
-        </tr>
+    
       </table>
     </>
   )
